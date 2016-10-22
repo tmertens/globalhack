50cdd6a6367aa7ad5dd7528c865cd3876098ca8d
Zach Taylor <taylorzr@gmail.com>
2016-10-21 20:48:22 -0500

    Connect clients and service providers

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,4 +4,7 @@ class Client < ApplicationRecord
     female: 1,
     other:  2
   }
+
+  has_many :client_service_providers
+  has_man :service_providers, through: :client_service_providers
 end
--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -1,2 +1,4 @@
 class ServiceProvider < ApplicationRecord
+  has_many :client_service_providers
+  has_man :clients, through: :client_service_providers
 end
