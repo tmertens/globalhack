a37356dfab750b760b7f782f3a436054788bc354
Nicole Lopez <nmlopez21@gmail.com>
2016-10-22 12:15:10 -0500

    associate users and orgs with owner

--- app/models/organization/create.rb
+++ app/models/organization/create.rb
@@ -4,11 +4,11 @@ class Organization < ApplicationRecord
   class Create
     include Verbalize
 
-    input :name
+    input :name, :owner
 
     def call
       fail!('An organization requires a name') unless name.present?
-      Organization.create!(name: name)
+      Organization.create!(name: name, owner: owner)
     end
   end
 end
