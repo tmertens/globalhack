18764bbdebcb627b6e8318ff3d849fe1f2916a11
Gabe Saravia <missinghandle@users.noreply.github.com>
2016-10-22 01:23:27 -0500

    fix

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,12 +1,8 @@
 class Client < ApplicationRecord
-  enum gender: {
-    male:   0,
-    female: 1,
-    other:  2
-  }
+  has_one :person
 
   has_many   :dependent_clients, class_name: 'Client', foreign_key: 'primary_client_id'
-  belongs_to :primary_client, class_name: 'Client'
+  belongs_to :primary_client,    class_name: 'Client'
 
   has_many :clients_organizations
   has_many :organizations, through: :clients_organizations
