986c775cd0bd498c4499233128092ea83061b62a
Zach Taylor <taylorzr@gmail.com>
2016-10-21 20:42:01 -0500

    Address

---
+++ db/migrate/20161022013745_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :addresses do |t|
+      t.string :city
+      t.float :latitude
+      t.string :state
+      t.string :street_1
+      t.string :street_2
+      t.string :zipcode
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,17 @@ ActiveRecord::Schema.define(version: 20161022020237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "addresses", force: :cascade do |t|
+    t.string   "city"
+    t.float    "latitude"
+    t.string   "state"
+    t.string   "street_1"
+    t.string   "street_2"
+    t.string   "zipcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
