8d8851a598b93f87ca2eac3eb33678d539383f0e
Paulo Gonzalez <paulo.gonzalez@avant.com>
2016-10-21 23:51:20 -0500

    Add spec for full_address

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
+  let(:address) do
+    Address.create!(street_1: '1 S Compton Ave',
+                    city: 'St Louis',
+                    state: "MO",
+                    zipcode: 63103)
+  end
+
+  context '#full_street_address' do
+    it 'displays the full address' do
+      result = address.full_street_address
+      expect(result).to eq('1 S Compton Ave,,St Louis,MO,63103')
+    end
+  end
 end
