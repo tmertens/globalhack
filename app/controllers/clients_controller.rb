60d586be03d6cc953a072d298f83f446385dfa08
Paulo Gonzalez <paulo.gonzalez@avant.com>
2016-10-22 14:16:02 -0500

    show page for clients works

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -7,7 +7,7 @@ class ClientsController < ApplicationController
   def create
     @client = Client.new(client_params)
     if @client.save
-      @client.client_profile = ClientProfile.new
+      @client.client_profile = ClientProfile.create!(client_id: @client.id, username: @client.informal_name)
       redirect_to @client
     else
       render :new
@@ -18,7 +18,17 @@ class ClientsController < ApplicationController
     @client = Client.find_by(id: params['id'])
   end
 
+  def update
+    @client = Client.find_by(id: params['id'])
+    @client.client_profile.avatar = params['avatar']
+    if @client.save
+      redirect_to @client
+    else
+      render 'edit'
+    end
+  end
+
   def client_params
-    params.require(:client).permit(:informal_name)
+    params.require(:client).permit(:informal_name, :avatar)
   end
 end
