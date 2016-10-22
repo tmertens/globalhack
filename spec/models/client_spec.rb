1272fb1592ba6853ceac278a58ec3240d4d203ad
Gabe Saravia <missinghandle@users.noreply.github.com>
2016-10-22 06:48:22 -0500

    need to remove these b/c no foreign key

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -3,6 +3,4 @@ require 'rails_helper'
 RSpec.describe Client, type: :model do
   it { is_expected.to have_many(:organizations).through(:clients_organizations) }
   it { is_expected.to have_many :contacts }
-  it { is_expected.to have_many :dependent_clients }
-  it { is_expected.to belong_to :primary_client }
 end
