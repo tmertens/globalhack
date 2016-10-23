ab2fc6ba5a1beed155cdb653443121392e9d8d23
Tim Mertens <tim@filmchicago.org>
2016-10-22 22:55:14 -0500

    Initial org setup flow

--- app/assets/javascripts/admin_dashboard.js
+++ app/assets/javascripts/admin_dashboard.js
@@ -1,3 +1,11 @@
 // Add files to be required for the dashboard here
 //
+//= require gentelella/vendors/nprogress/nprogress.js
+//= require gentelella/vendors/moment/min/moment.min.js
+//= require gentelella/vendors/bootstrap-daterangepicker/daterangepicker.js
+//= require gentelella/vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js
+//= require gentelella/vendors/google-code-prettify/src/prettify.js
+//= require gentelella/vendors/switchery/dist/switchery.min.js
+//= require gentelella/vendors/jQuery-Smart-Wizard/js/jquery.smartWizard.js
+//= require gentelella/vendors/select2/dist/js/select2.full.min.js
 
--- app/assets/javascripts/dashboard/late_load.js
+++ app/assets/javascripts/dashboard/late_load.js
@@ -1,3 +1,2 @@
-//= require gentelella/vendors/nprogress/nprogress.js
-//= require gentelella/vendors/jQuery-Smart-Wizard/js/jquery.smartWizard.js
 //= require gentelella/build/js/custom.min.js
+
